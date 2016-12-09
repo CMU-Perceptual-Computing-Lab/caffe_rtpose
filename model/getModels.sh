ebf48a8153022db5a7ab22e90e732b178ee863e5
tsimon <tsimon@cs.cmu.edu>
2016-12-09 10:27:52 -0500

    Fix models path in getModels.sh

--- model/getModels.sh
+++ model/getModels.sh
@@ -3,7 +3,7 @@
 [[ ! -f coco/pose_iter_440000.caffemodel ]] && wget http://posefs1.perception.cs.cmu.edu/Users/tsimon/Projects/coco/data/models/coco/pose_iter_440000.caffemodel -P coco/  
 
 # MPI
-[[ ! -f mpi/pose_iter_264000.caffemodel ]] && wget http://posefs1.perception.cs.cmu.edu/Users/tsimon/Projects/coco/data/models/coco/pose_iter_440000.caffemodel -P mpi/   
+[[ ! -f mpi/pose_iter_264000.caffemodel ]] && wget http://posefs1.perception.cs.cmu.edu/Users/tsimon/Projects/coco/data/models/mpi/pose_iter_160000.caffemodel -P mpi/   
 
 # MPI single level
 #[[ ! -f mpi_1l/pose_iter_164000.caffemodel ]] && scp ${SERVER}:/media/posenas1b/Users/zhe/arch/MPI_exp_caffe/pose43/exp08/model/pose_iter_164000.caffemodel mpi_1l/
